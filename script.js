f082ed0bff2b9a463f11421349d0533b1aa6bd7b
DevJyotiPixel <mohantyjyoti88@gmail.com>
2026-06-05 17:37:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector(".card").addEventListener("click",() =>{
+    const card = document.querySelector(".card");
+    card.style.animation = "none";
+    setTimeout(() => {
+        
+    }, 5)
+});
